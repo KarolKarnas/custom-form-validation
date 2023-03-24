3f42f2d630253df779640380ca59249c16411200
KarolKarnas <karol.karnas@gmail.com>
2023-03-24 13:24:17 +0100

    Add Zipcode
    Zipcode is disabled by default
    When in country input the correct country is filled in then zipcode became not disabled.
    
    Zip Code validates on RegExp fetched from the object, based on the country name introduced by the user. So every country is validated with it's own ZIP code.
    
    Additionaly format of the zip i displaying on input. Also fetched from the object

--- app.js
+++ app.js
@@ -9,6 +9,9 @@ const errorEmail = email.nextElementSibling;
 const country = document.getElementById('country');
 const errorCountry = country.nextElementSibling;
 
+const zip = document.getElementById('zipcode');
+const errorZip = zip.nextElementSibling;
+
 // As per the HTML Specification
 const emailRegExp =
 	/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
@@ -33,99 +36,123 @@ email.addEventListener('input', () => {
 		errorEmail.textContent = '';
 		errorEmail.className = 'error';
 	} else if (emailRegExp.test(email.value)) {
-        email.className = 'valid';
+		email.className = 'valid';
 		errorEmail.textContent = 'email good';
 		errorEmail.className = 'error good';
-    }
-    else {
+	} else {
 		email.className = 'invalid';
-        errorEmail.className = 'error';
+		errorEmail.className = 'error';
 		errorEmail.textContent = 'you are almost there';
 	}
 });
-
-
+let countryMatch;
+let zipRegExp;
+let zipFormat
 
 country.addEventListener('input', () => {
+	countryMatch = false;
+	countryData.forEach((el) => {
+		if (el.Country.toLowerCase() === country.value.toLowerCase()) {
+			countryMatch = true;
+			zipRegExp = new RegExp(el.Regex);
+            zipFormat = el.Format;
+		}
+	});
 
-    let countryMatch;
-    countryData.forEach((el) => {
-        if (el.Country.toLowerCase() === country.value.toLowerCase()) {
-            console.log(el.Regex);
-            countryMatch = true;
-        }
-    });
-	
 	if (country.value.length === 0) {
 		country.className = 'valid';
 		errorCountry.textContent = '';
 		errorCountry.className = 'error';
+		zip.disabled = true;
 	} else if (countryMatch) {
-        country.className = 'valid';
+		country.className = 'valid';
 		errorCountry.textContent = 'good country';
 		errorCountry.className = 'error good';
-    }
-    else {
+		zip.disabled = false;
+	} else {
 		country.className = 'invalid';
-		errorCountry.textContent = 'country you are almost there';
-        errorCountry.className = 'error';
+		errorCountry.textContent = 'country you are almost there.';
+		errorCountry.className = 'error';
+		zip.disabled = true;
 	}
 });
 
-// This defines what happens when the user tries to submit the data
-form.addEventListener('submit', (event) => { 
-	// const isEmailValid =
-	// 	email.value.length === 0 || emailRegExp.test(email.value);
-	// if (!isEmailValid) {
-	// 	event.preventDefault();
-	// 	email.className = 'invalid';
-	// 	errorEmail.textContent = 'I expect an email, darling!';
-	// 	errorEmail.className = 'error active';
-	// } else {
-	// 	email.className = 'valid';
-	// 	errorEmail.textContent = '';
-	// 	errorEmail.className = 'error';
-	// }
-//MAIL
-if (email.value.length === 0) {
-    email.className = 'valid';
-    errorEmail.textContent = '';
-    errorEmail.className = 'error';
-} else if (emailRegExp.test(email.value)) {
-    email.className = 'valid';
-    errorEmail.textContent = '';
-    errorEmail.className = 'error';
-
-}
-else {
-    event.preventDefault()
-    email.className = 'invalid';
-    errorEmail.className = 'error active';
-    errorEmail.textContent = 'This is not an email';
-}
-//COUNTRY
-let countryMatch;
-countryData.forEach((el) => {
-    if (el.Country.toLowerCase() === country.value.toLowerCase()) {
-        console.log(el.Regex);
-        countryMatch = true;
-    }
+zip.addEventListener('input', () => {
+	if (zip.value.length === 0) {
+		zip.className = 'invalid';
+		errorZip.textContent = '';
+		errorZip.className = 'error';
+	} else if (zipRegExp.test(zip.value)) {
+		zip.className = 'valid';
+		errorZip.textContent = 'ZIP good';
+		errorZip.className = 'error good';
+	} else {
+		zip.className = 'invalid';
+		errorZip.className = 'error';
+		errorZip.textContent = `you are almost there. The format is: ${zipFormat}`;
+	}
 });
 
-if (country.value.length === 0) {
-    country.className = 'valid';
-    errorCountry.textContent = '';
-    errorCountry.className = 'error';
-} else if (countryMatch) {
-    country.className = 'valid';
-    errorCountry.textContent = '';
-    errorCountry.className = 'error';
-}
-else {
-    event.preventDefault()
-    country.className = 'invalid';
-    errorCountry.textContent = 'We do not have such country on a list. Use official name. And not schortcuts.';
-    errorCountry.className = 'error active';
-}
+// ------------------ SUBMIT ----------------------
+form.addEventListener('submit', (event) => {
+	//MAIL
+	if (email.value.length === 0) {
+		email.className = 'valid';
+		errorEmail.textContent = '';
+		errorEmail.className = 'error';
+	} else if (emailRegExp.test(email.value)) {
+		email.className = 'valid';
+		errorEmail.textContent = '';
+		errorEmail.className = 'error';
+	} else {
+		event.preventDefault();
+		email.className = 'invalid';
+		errorEmail.className = 'error active';
+		errorEmail.textContent = 'This is not an email';
+	}
+	//COUNTRY
+	country.addEventListener('input', () => {
+		countryMatch = false;
+		countryData.forEach((el) => {
+			if (el.Country.toLowerCase() === country.value.toLowerCase()) {
+				countryMatch = true;
+				zipRegExp = new RegExp(el.Regex);
+			}
+		});
 
+		if (country.value.length === 0) {
+			country.className = 'valid';
+			errorCountry.textContent = '';
+			errorCountry.className = 'error';
+			zip.disabled = true;
+		} else if (countryMatch) {
+			country.className = 'valid';
+			errorCountry.textContent = '';
+			errorCountry.className = 'error';
+			zip.disabled = false;
+		} else {
+			country.className = 'invalid';
+			errorCountry.textContent =
+				'We do not have such country on a list. Use official name. And not schortcuts.';
+			errorCountry.className = 'error active';
+			zip.disabled = true;
+		}
+	});
+
+	//ZiPP
+	if (zip.disabled === true) {
+	} else if (!zip.disabled && zip.value.length === 0) {
+		zip.className = 'invalid';
+		errorZip.textContent = '';
+		errorZip.className = 'error';
+	} else if (!zip.disabled && zipRegExp.test(zip.value)) {
+		zip.className = 'valid';
+		errorZip.textContent = '';
+		errorZip.className = 'error';
+	} else if (!zip.disabled && !(zipRegExp.test(zip.value)) ) {
+		event.preventDefault();
+		zip.className = 'invalid';
+		errorZip.className = 'error';
+		errorZip.textContent = `Give me good ZIP or give me nothing! And the good ZIP for your country looks like that: ${zipFormat}`;
+	}
 });
