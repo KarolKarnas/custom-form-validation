2f48b336575dca675a624bbf086150a83bc57f22
KarolKarnas <karol.karnas@gmail.com>
2023-03-24 16:53:55 +0100

    Add password and password confirmation inputs
    Password check with RegExp,must have:
               - 1 uppercase letter
               - 1 lowercase letter
               - 1 number
               - 1 special character (@, $, !, %, *, ?, or &)
               - at least 8 characters long`;
    Confirmation checks are they the same.
    Display Thank You message after success, and hide it with setTimeout.
    
    Make email, password and confirmation obligatory

--- app.js
+++ app.js
@@ -12,9 +12,25 @@ const errorCountry = country.nextElementSibling;
 const zip = document.getElementById('zipcode');
 const errorZip = zip.nextElementSibling;
 
+const password = document.getElementById('password');
+const errorPassword = password.nextElementSibling;
+
+const confirm = document.getElementById('confirm');
+const errorConfirm = confirm.nextElementSibling;
+
+const iconPassword = document.getElementById('icon-password')
+const iconConfirm = document.getElementById('icon-confirm')
+
+const thankYou = document.getElementById('thank-you');
+let displayFlag = false
+
+
 // As per the HTML Specification
 const emailRegExp =
 	/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+const passwordRegExp =
+	/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
 
 // Now we can rebuild our validation constraint
 // Because we do not rely on CSS pseudo-class, we have to
@@ -32,8 +48,8 @@ window.addEventListener('load', () => {
 // This defines what happens when the user types in the field
 email.addEventListener('input', () => {
 	if (email.value.length === 0) {
-		email.className = 'valid';
-		errorEmail.textContent = '';
+		email.className = 'invalid';
+		errorEmail.textContent = 'Sir, we need your email';
 		errorEmail.className = 'error';
 	} else if (emailRegExp.test(email.value)) {
 		email.className = 'valid';
@@ -47,15 +63,18 @@ email.addEventListener('input', () => {
 });
 let countryMatch;
 let zipRegExp;
-let zipFormat
+let zipFormat;
 
 country.addEventListener('input', () => {
 	countryMatch = false;
+	zip.value = '';
+	errorZip.textContent = '';
+	errorZip.className = 'error';
 	countryData.forEach((el) => {
 		if (el.Country.toLowerCase() === country.value.toLowerCase()) {
 			countryMatch = true;
 			zipRegExp = new RegExp(el.Regex);
-            zipFormat = el.Format;
+			zipFormat = el.Format;
 		}
 	});
 
@@ -69,6 +88,8 @@ country.addEventListener('input', () => {
 		errorCountry.textContent = 'good country';
 		errorCountry.className = 'error good';
 		zip.disabled = false;
+		zip.className = 'invalid';
+		errorZip.textContent = `you are almost there. The format is: ${zipFormat}`;
 	} else {
 		country.className = 'invalid';
 		errorCountry.textContent = 'country you are almost there.';
@@ -93,13 +114,63 @@ zip.addEventListener('input', () => {
 	}
 });
 
+password.addEventListener('input', () => {
+	 if (passwordRegExp.test(password.value)) {
+		password.className = 'valid';
+		errorPassword.textContent = 'Pass good enought';
+		errorPassword.className = 'error good';
+	} else {
+		password.className = 'invalid';
+		errorPassword.className = 'error';
+		errorPassword.textContent = `Almost. Pass must heve:
+        - 1 uppercase letter
+        - 1 lowercase letter
+        - 1 number 
+        - 1 special character (@, $, !, %, *, ?, or &)
+        - at least 8 characters long`;
+	}
+});
+
+confirm.addEventListener('input', () => {
+    if (confirm.value === password.value) {
+		confirm.className = 'valid';
+		errorConfirm.textContent = 'They are identicall';
+		errorConfirm.className = 'error good';
+	} else {
+		confirm.className = 'invalid';
+		errorConfirm.className = 'error';
+		errorConfirm.textContent = `the pass must be identicall`;
+	}
+})
+
+
+iconPassword.addEventListener('click', () => {
+    if (password.type === 'text') {
+        password.type = 'password'
+        iconPassword.textContent = "visibility"
+    } else {
+        password.type = 'text'
+        iconPassword.textContent = "visibility_off"
+    }
+})
+iconConfirm.addEventListener('click', () => {
+    if (confirm.type === 'text') {
+        confirm.type = 'password'
+        iconConfirm.textContent = "visibility"
+    } else {
+        confirm.type = 'text'
+        iconConfirm.textContent = "visibility_off"
+    }
+})
+
 // ------------------ SUBMIT ----------------------
 form.addEventListener('submit', (event) => {
 	//MAIL
 	if (email.value.length === 0) {
-		email.className = 'valid';
-		errorEmail.textContent = '';
-		errorEmail.className = 'error';
+		email.className = 'invalid';
+		errorEmail.textContent = 'Hello, we need your email';
+		errorEmail.className = 'error active';
+        event.preventDefault()
 	} else if (emailRegExp.test(email.value)) {
 		email.className = 'valid';
 		errorEmail.textContent = '';
@@ -149,10 +220,54 @@ form.addEventListener('submit', (event) => {
 		zip.className = 'valid';
 		errorZip.textContent = '';
 		errorZip.className = 'error';
-	} else if (!zip.disabled && !(zipRegExp.test(zip.value)) ) {
+	} else if (!zip.disabled && !zipRegExp.test(zip.value)) {
 		event.preventDefault();
 		zip.className = 'invalid';
 		errorZip.className = 'error';
 		errorZip.textContent = `Give me good ZIP or give me nothing! And the good ZIP for your country looks like that: ${zipFormat}`;
 	}
+
+
+    //PASSWORD
+        if (passwordRegExp.test(password.value)) {
+           password.className = 'valid';
+           errorPassword.textContent = '';
+           errorPassword.className = 'error';
+       } else {
+        event.preventDefault();
+           password.className = 'invalid';
+           errorPassword.className = 'error active';
+           errorPassword.textContent = `Almost. Pass must have:
+           - 1 uppercase letter
+           - 1 lowercase letter
+           - 1 number 
+           - 1 special character (@, $, !, %, *, ?, or &)
+           - at least 8 characters long`;
+       }
+   
+       if (confirm.value.length !== 0 && confirm.value === password.value) {
+           confirm.className = 'valid';
+           errorConfirm.textContent = '';
+           errorConfirm.className = 'error good';
+           thankYou.style= 'inherit'
+
+           setTimeout(() => {
+               thankYou.style.display='none'
+           }, 5000);
+       } else if (confirm.value.length === 0) {
+        confirm.className = 'invalid';
+        errorConfirm.className = 'error active';
+        errorConfirm.textContent = `the confirm can not be empty Sir!`;
+        event.preventDefault()
+       }
+        else {
+        event.preventDefault();
+           confirm.className = 'invalid';
+           errorConfirm.className = 'error';
+           errorConfirm.textContent = `the pass must be identical`;
+       }
+ 
+event.preventDefault()
+
+
 });
